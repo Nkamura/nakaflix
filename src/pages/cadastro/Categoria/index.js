798c07ad0f7c543b30b1745fe590c47e0405fab3
Nkamura <gustavonakamura@alunos.utfpr.edu.br>
2020-07-29 15:59:54 -0300

    NomeDaCategoria Implementado

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from 'react-router-dom';
 
+
+
 function CadastroCategoria() {
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('  Valor Inicial');
+
+
     return (
         <PageDefault>
-            <h1>Pagina de Cadastro da Categoria</h1>
+            <h1>Cadastro de Categoria: {nomeDaCategoria} </h1>
+
+            <form>
+                <label>
+                    Nome da Categoria:
+                <input
+                        type="text"
+                        value={nomeDaCategoria}
+                        onChange={function funcaoHandler(infosDoEvento) {
+                            console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);
+                            setNomeDaCategoria(infosDoEvento.target.value);
+                        }}
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+        </button>
+            </form>
 
 
             <Link to="/">
                 Ir para home
       </Link>
-
         </PageDefault>
     )
 }
