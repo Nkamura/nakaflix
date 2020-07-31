61471e847f771057f2c546a8b226baaa215d341f
Nkamura <gustavonakamura@alunos.utfpr.edu.br>
2020-07-31 09:04:34 -0300

    Backend Deploy

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -35,7 +35,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://nakaflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
@@ -121,7 +121,7 @@ function CadastroCategoria() {
       {
         categorias.length === 0 && (
           <div>
-            Sem Nenhuma Categoria
+            Carregando ...
           </div>
         )
       }
